68a19f7b06da8743d4a09b7f8f74003ade3ac559
xinyuezhao <3040796317@qq.com>
2015-11-06 23:06:52 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('div[data-type="background"]').each(function(){
+        var $bgobj = $(this); // assigning the object
+        $window = $(window); 
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+            
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });    
+});
